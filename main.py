2e933514523ebe51a20073e3af7d0e9c5b391cf5
jeff-romero <jeffromero86@gmail.com>
2026-06-13 13:47:57 -0700

    add test for invalid password

--- main.py
+++ main.py
@@ -39,11 +39,30 @@ class TestLogin:
             print(f"{inspect.stack()[0][3]}: {e}")
         else:
             print(f"{inspect.stack()[0][3]}: PASS")
-            pass
-        finally:
-            driver.quit()
+
+    def test_invalid_password(self):
+        try:
+            load_home_page()
+
+            WebDriverWait(driver, WAIT_TIME_DEFAULT_SEC).until(
+                expected_conditions.presence_of_element_located((By.XPATH, PASSWORD_INPUT))
+            )
+
+            password_input = driver.find_element(By.XPATH, PASSWORD_INPUT)
+            password_input.clear()
+            password_input.send_keys("zxc456" + Keys.ENTER)
+
+            WebDriverWait(driver, WAIT_TIME_DEFAULT_SEC).until(
+                expected_conditions.text_to_be_present_in_element((By.XPATH, "//p[@class='error']"), "Please enter a username and password.")
+            )
+        except Exception as e:
+            print(f"{inspect.stack()[0][3]}: {e}")
+        else:
+            print(f"{inspect.stack()[0][3]}: PASS")
 
 
 if __name__ == "__main__":
     test_login = TestLogin()
     test_login.test_invalid_username()
+    test_login.test_invalid_password()
+    driver.quit()
